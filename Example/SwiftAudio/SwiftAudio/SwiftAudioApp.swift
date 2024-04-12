06854764f434e2d743cd154a3f8f629b6d9cd211
dcvz <david@dcvz.io>
2024-04-12 17:21:05 +0200

    Main screen in shared

---
+++ Example/SwiftAudio/SwiftAudio/SwiftAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAudioApp.swift
+//  SwiftAudio
+//
+//  Created by Brandon Sneed on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PlayerView()
+        }
+    }
+}
